b8cbc9a550a64d181c508d3a44e8f87023c43cf6
Veronica <veronica.ardilao@outlook.es>
2022-12-08 23:34:38 -0500

    capturing button id in Javascript

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//Variables initialization
+let cards = 0;
+let card1 = null;
+
+//Ramdom numbers generation
 let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numbers = numbers.sort(()=>{return Math.random()-0.5})
+console.log(numbers)
+//Main Function
+function cardsClick(id) {
+    cards++;
+    console.log(cards);
+
+    if(cards == 1){
+        card1 = document.getElementById(id);
+        card1.innerHTML = numbers[id];
+    }
+}
